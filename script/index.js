3141e868552547cf87bbc77eeb6f05f1c63a33dc
HordynskyiVitalii <145695512+HordynskyiVitalii@users.noreply.github.com>
2024-01-13 21:06:59 +0100

    add

--- script/index.js
+++ script/index.js
@@ -29,3 +29,27 @@ $(document).ready(function () {
         pauseOnDotHover:true,
     });
 });
+
+$(document).ready(function () {
+    $('.card__images').slick({
+        arrows:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        speed:1000,
+        autoplay:true,
+        autoPlaySpeed:1500,
+        pauseOnFocus:true,
+        pauseOnHover:true,
+        pauseOnDotHover:true,
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var slickPrev = document.querySelector('.slick-prev');
+    var slickNext = document.querySelector('.slick-next');
+
+    if (slickPrev && slickNext) {
+        slickPrev.style.display = 'none';
+        slickNext.style.display = 'none';
+    }
+});
